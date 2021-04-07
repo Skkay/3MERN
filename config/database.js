0f199afba7ed02052a59c7c0839270de850b3d3a
Skkay <skkay.thomas@gmail.com>
2021-04-07 14:24:22 +0200

    Code formatter

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const path = require('path')
+const path = require("path");
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wim0n.mongodb.net/mern_project`;
 
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
 
-require('dotenv').config({ path: path.join(__dirname, 'config/.env') })
-require('dotenv').config({ path: path.join(__dirname, 'config/.env.local') })
-require('./config/database.js')
+require("dotenv").config({ path: path.join(__dirname, "config/.env") });
+require("dotenv").config({ path: path.join(__dirname, "config/.env.local") });
+require("./config/database.js");
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
-})
+  console.log(`Listening on http://localhost:${port}`);
+});
