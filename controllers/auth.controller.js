8605bc2a4191c5d0a2611a7700e5af400dca7622
Skkay <skkay.thomas@gmail.com>
2021-04-07 15:27:51 +0200

    Register users via '/user/register' route

---
+++ controllers/auth.controller.js
@@ -0,0 +1,17 @@
+const UserModel = require("../models/user.model");
+
+module.exports.signUp = async (req, res) => {
+  const { email, firstName, lastName, password } = req.body;
+
+  try {
+    const user = await UserModel.create({
+      email,
+      firstName,
+      lastName,
+      password,
+    });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+};
--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const userRoutes = require("./routes/user.routes");
+
 require("dotenv").config({ path: path.join(__dirname, "config/.env") });
 require("dotenv").config({ path: path.join(__dirname, "config/.env.local") });
 require("./config/database.js");
 
 const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
