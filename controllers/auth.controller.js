b9b8d4b8f481ef988b384c54704fa3c7dc395d65
Skkay <skkay.thomas@gmail.com>
2021-04-07 16:45:00 +0200

    User login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,12 @@
 const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const tokenDuration = 24 * 3600 * 1000;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: tokenDuration,
+  });
+};
 
 module.exports.signUp = async (req, res) => {
   const { email, firstName, lastName, password } = req.body;
@@ -11,6 +19,20 @@ module.exports.signUp = async (req, res) => {
       password,
     });
     res.status(201).json({ user: user._id });
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+};
+
+module.exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+
+    res.cookie("jwt", token, { httpOnly: true, maxAge: tokenDuration });
+    res.status(200).json({ token: token });
   } catch (err) {
     res.status(400).send({ err });
   }
