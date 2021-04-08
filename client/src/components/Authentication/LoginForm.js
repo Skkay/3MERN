8b4954aee6c893e2ba197b682b26c12097e2d306
Skkay <skkay.thomas@gmail.com>
2021-04-08 20:09:31 +0200

    Login form component

---
+++ client/src/components/Authentication/LoginForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div className="card-body">
+      <h4>Login</h4>
+      <hr />
+      <form>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
