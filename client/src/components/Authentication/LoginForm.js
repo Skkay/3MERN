a02dfea2892b6666115c5ca219198e606a861c7e
Skkay <skkay.thomas@gmail.com>
2021-04-09 00:20:50 +0200

    Login form: post data to API

--- client/src/components/Authentication/LoginForm.js
+++ client/src/components/Authentication/LoginForm.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API_URL}/user/login`,
+      withCredentials: true,
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+    .then((res) => {
+      window.location = "/";
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  };
+
   return (
     <div className="card-body">
       <h4>Login</h4>
       <hr />
-      <form>
+      <form action="" onSubmit={handleLogin} id="loginForm">
         <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <label for="email">Email address</label>
+          <input type="email" className="form-control" id="email" onChange={(e) => setEmail(e.target.value)}/>
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
+          <label for="password">Password</label>
+          <input type="password" className="form-control" id="password" onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
