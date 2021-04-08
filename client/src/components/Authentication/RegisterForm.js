49f700a88bd47517005d010bbe8fa2509f697334
Skkay <skkay.thomas@gmail.com>
2021-04-09 00:38:54 +0200

    Register form: post data to API

--- client/src/components/Authentication/RegisterForm.js
+++ client/src/components/Authentication/RegisterForm.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const RegisterForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API_URL}/user/register`,
+      withCredentials: true,
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      },
+    })
+    .then((res) => {
+      window.location = "/login";
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+
   return (
     <div className="card-body">
       <h4>Register</h4>
       <hr />
-      <form>
+      <form action="" onSubmit={handleRegister} id="registerForm">
         <div className="form-group">
           <label for="firstname">First name</label>
-          <input type="text" className="form-control" id="firstname" />
+          <input type="text" className="form-control" id="firstname" required onChange={(e) => setFirstName(e.target.value)} />
         </div>
         <div className="form-group">
           <label for="lastname">Last name</label>
-          <input type="text" className="form-control" id="lastname" />
+          <input type="text" className="form-control" id="lastname" required onChange={(e) => setLastName(e.target.value)} />
         </div>
         <div className="form-group">
           <label for="email">Email address</label>
-          <input type="email" className="form-control" id="email" />
+          <input type="email" className="form-control" id="email" required onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="form-group">
           <label for="password">Password</label>
-          <input type="password" className="form-control" id="password" />
+          <input type="password" className="form-control" id="password" required onChange={(e) => setPassword(e.target.value)} />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
