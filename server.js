3faf532bd115673b2421962a42b3823d898a09d4
Skkay <skkay.thomas@gmail.com>
2021-04-08 23:53:09 +0200

    Revert "API url start now with "/api""
    This reverts commit b148ec4b0f806f6011b473c7a54f5b8c0d099a9e.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/require_auth", requireAuth, (req, res) => {
 });
 
 app.use("/", homeRoutes);
-app.use("/api//user", userRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
