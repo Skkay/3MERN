cf9a17147f9d01530e85fd9ec50ad52b8c6b41d4
Skkay <skkay.thomas@gmail.com>
2021-04-08 15:20:49 +0200

    Move router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import "./styles/bootstrap.min.css";
-import Routes from "./components/Routes";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import LoginView from "./pages/LoginView";
 
 function App() {
   return (
     <div>
-      <Routes />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={LoginView} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
