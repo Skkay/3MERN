5dc5af95eaef43c51d07476814244d14c337949f
Skkay <skkay.thomas@gmail.com>
2021-04-10 00:49:17 +0200

    Add search bar to home page to find cities

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card/Card"
 
 const Home = () => {
+  const [search, setSearch] = useState('');
+
   return (
     <div>
+      <form className="mx-3" action="">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" onChange={(e) => setSearch(e.target.value)} />
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button">Search</button>
+          </div>
+        </div>
+      </form>
       <Card />
     </div>
   );
