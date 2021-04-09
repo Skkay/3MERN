ab287fa46a161c59b32bb72b26cbf3b4d08dc213
Skkay <skkay.thomas@gmail.com>
2021-04-09 16:11:37 +0200

    Navbar links to home page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -27,14 +27,14 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
       <div className="container">
-        <a className="navbar-brand" href="#">3MERN</a>
+        <a className="navbar-brand" href="/">3MERN</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                  <a className="nav-link" href="#">Home</a>
+                  <a className="nav-link" href="/">Home</a>
               </li>
             </ul>
             {uid ? (
