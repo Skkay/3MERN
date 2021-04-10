63daa09fcc5267e670463a7b43bfa1252b0af742
Skkay <skkay.thomas@gmail.com>
2021-04-10 17:07:32 +0200

    Get and show weather data from props

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -5,21 +5,21 @@ const Card = (props) => {
   const [liked, setLiked] = useState(true);
 
   return (
-    <div className="card m-3" style={{width: width}}>
+    <div className="card m-3" style={{width: width}}>  
       <div className="card-body">
         <div className="row">
           <div className="col-8 col-sm-6">
-            <h5 className="card-title"><button className="ButtonLink p-0 mr-2"><i className={liked ? "bi bi-heart-fill" : "bi bi-heart"} style={{color: "#E0245E"}} /></button>Paris, FR</h5>
-            <p className="card-text text-muted text-capitalize">broken clouds</p>
-            <h1>20°C</h1>
-            <p className="m-0 font-weight-bold">Feels like: 18°C</p>
-            <p className="m-0">Min: 9°C</p>
-            <p className="m-0">Max: 23°C</p>
+            <h5 className="card-title"><button className="ButtonLink p-0 mr-2"><i className={liked ? "bi bi-heart-fill" : "bi bi-heart"} style={{color: "#E0245E"}} /></button>{props.name}, {props.country}</h5>
+            <p className="card-text text-muted text-capitalize">{props.weatherDescription}</p>
+            <h1>{Math.round(props.temp - 273.15)}°C</h1>
+            <p className="m-0 font-weight-bold">Feels like: {Math.round(props.tempFeelsLike - 273.15)}°C</p>
+            <p className="m-0">Min: {Math.round(props.tempMin - 273.15)}°C</p>
+            <p className="m-0">Max: {Math.round(props.tempMax - 273.15)}°C</p>
             <hr />
-            <p className="m-0">Humidity: 55</p>
-            <p className="m-0">Wind speed: 5.14 km/h, 230°</p>
+            <p className="m-0">Humidity: {props.humidity}%</p>
+            <p className="m-0">Wind speed: {props.windSpeed} km/h, {['N','NE', 'E','SE', 'S','SW', 'W','NW'][Math.floor(((props.windDir+22.5)%360)/45)]}</p>
             <hr />
-            <p className="m-0">00h00 - 00h00</p>
+            <p className="m-0">{new Date(props.sunrise * 1000).toLocaleTimeString().slice(0, -3)} - {new Date(props.sunset * 1000).toLocaleTimeString().slice(0, -3)}</p>
           </div>
           <div className="col-4 col-sm-6">
             <img src={process.env.REACT_APP_PUBLIC_URL + '/assets/weather_icons/cloudy_light.png'} alt="cloudy" className="img-thumbnail" />
