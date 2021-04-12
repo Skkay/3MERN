3fd251bb0eab9d796c945586ada74ce636e951c6
Skkay <skkay.thomas@gmail.com>
2021-04-12 15:40:13 +0200

    addToFavorite route to save city to user's favorite cities

---
+++ controllers/user.controller.js
@@ -0,0 +1,30 @@
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+module.exports.addToFavorite = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(400).send("Unknown user id: " + req.params.id);
+  }
+
+  try {
+    await UserModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $addToSet: { favoriteCities: req.body.cityId }
+      },
+      {
+        new: true, upsert: true
+      },
+      (err, doc) => {
+        if (!err) {
+          res.status(204).json(doc)
+        } else {
+          res.status(400).send({ err });
+        }
+      }
+    );
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+}
+
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logOut);
+router.post("/addToFavorite/:id", userController.addToFavorite);
 
 module.exports = router;
