56ec7f9d089eaff0f91685609b95848eb17f9882
Skkay <skkay.thomas@gmail.com>
2021-04-14 15:57:09 +0200

    CardDetail.js with props values

--- client/src/components/Card/CardDetail.js
+++ client/src/components/Card/CardDetail.js
@@ -19,14 +19,14 @@ const CardDetail = (props) => {
       <div className="card-body">
         <div className="row">
           <div className="col-8 col-sm-6">
-            <h5 className="card-title">Jeudi 15 avril</h5>
-            <p className="card-text text-muted text-capitalize">Clear sky</p>
-            <h1>{Math.round(285 - 273.15)}°C <span className="text-muted">{Math.round(275 - 273.15)}°C</span></h1>
+            <h5 className="card-title">{new Date(props.timestamp * 1000).toLocaleDateString('en-US', {weekday: 'long', month: 'long', day: 'numeric'})}</h5>
+            <p className="card-text text-muted text-capitalize">{props.weatherDescription}</p>
+            <h1>{Math.round(props.tempMax - 273.15)}°C <span className="text-muted">{Math.round(props.tempMin - 273.15)}°C</span></h1>
             <hr />
-            <p className="m-0">Humidity: 52%</p>
-            <p className="m-0">Wind: 7 km/h, {['N','NE', 'E','SE', 'S','SW', 'W','NW'][Math.floor(((53+22.5)%360)/45)]}</p>
+            <p className="m-0">Humidity: {props.humidity}%</p>
+            <p className="m-0">Wind: {props.windSpeed} km/h, {['N','NE', 'E','SE', 'S','SW', 'W','NW'][Math.floor(((props.windDir+22.5)%360)/45)]}</p>
             <hr />
-            <p className="m-0">{new Date(1618376955 * 1000).toLocaleTimeString().slice(0, -3)} - {new Date(1618425792 * 1000).toLocaleTimeString().slice(0, -3)}</p>
+            <p className="m-0">{new Date(props.sunrise * 1000).toLocaleTimeString().slice(0, -3)} - {new Date(props.sunset * 1000).toLocaleTimeString().slice(0, -3)}</p>
           </div>
           <div className="col-4 col-sm-6">
             <img src={`${process.env.REACT_APP_PUBLIC_URL}/assets/weather_icons/${img}_light.png`} alt={img} className="img-thumbnail" />
