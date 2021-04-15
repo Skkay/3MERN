7cb1984643278417b3d56ce8863cfb7c364d754c
Skkay <skkay.thomas@gmail.com>
2021-04-16 00:39:49 +0200

    Set short date format if screen width is less than 435px

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UidContext } from "../components/AppContext";
 import axios from 'axios';
 import cookie from "js-cookie";
@@ -7,6 +7,15 @@ import Clock from "react-live-clock";
 
 const Navbar = () => {
   const uid = useContext(UidContext);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, [])
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  }
 
   const removeCookie = (key) => {
     if (window !== "undefined") {
@@ -30,10 +39,14 @@ const Navbar = () => {
     <nav className="navbar navbar-expand navbar-light bg-light mb-4">
       <div className="mx-auto text-center">
         <ul className="navbar-nav">
-          <li className="nav-item"><a className="nav-link ButtonLink" href="/">Home</a></li>
+          <li className="nav-item" style={{width: "80px"}}><a className="nav-link ButtonLink" href="/">Home</a></li>
           <li className="nav-item mx-4">
             <dl>
-              <li><Clock format={'dddd, MMMM D'} ticking={true} /></li>
+              {windowWidth < 435 ? (
+                <li><Clock format={'ddd, MMM D'} ticking={true} /></li>
+              ) : (
+                <li><Clock format={'dddd, MMMM D'} ticking={true} /></li>
+              )}
               <li><Clock format={'HH:mm:ss'} ticking={true} /></li>
             </dl>
           </li>
