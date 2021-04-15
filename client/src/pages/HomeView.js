3ce32d71740ebf05f9c49dc8dfbffdb069a003e4
Skkay <skkay.thomas@gmail.com>
2021-04-15 15:58:50 +0200

    Fetch favorite cities weather data from connected user

--- client/src/pages/HomeView.js
+++ client/src/pages/HomeView.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { UidContext } from "../components/AppContext";
+import Card from "../components/Card/Card"
 
 const Home = () => {
+  const uid = useContext(UidContext);
+  const [endFetch, setEndFetch] = useState(false);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    if (!uid) return;
+
+    fetch(`${process.env.REACT_APP_API_URL}/user/getFavoriteCitiesWeatherData/${uid}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setWeatherData(responseJson);
+        setEndFetch(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }, [uid]);
+
   return (
     <div>
       Home page
